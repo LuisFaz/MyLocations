56d6d9d9047e45eb77eba9ceb87bc0fcba47cf2f
LuisFaz <alejandro.faz@hotmail.com>
2024-02-11 17:08:09 -0500

    Added New Tag Location View

--- LocationDetailsViewController.swift
+++ LocationDetailsViewController.swift
@@ -5,4 +5,19 @@
 //  Created by Luis Faz on 2/11/24.
 //
 
-import Foundation
+import UIKit
+class LocationDetailsViewController: UITableViewController {
+  @IBOutlet var descriptionTextView: UITextView!
+  @IBOutlet var categoryLabel: UILabel!
+  @IBOutlet var latitudeLabel: UILabel!
+  @IBOutlet var longitudeLabel: UILabel!
+  @IBOutlet var addressLabel: UILabel!
+  @IBOutlet var dateLabel: UILabel!
+  // MARK: - Actions
+  @IBAction func done() {
+    navigationController?.popViewController(animated: true)
+  }
+  @IBAction func cancel() {
+    navigationController?.popViewController(animated: true)
+  }
+}
