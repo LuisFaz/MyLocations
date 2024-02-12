b05f27aa818f8457b0cc8e915c4a83c3114a6d03
LuisFaz <alejandro.faz@hotmail.com>
2024-02-11 21:26:46 -0500

    Finish Cleaning the App

---
+++ MyLocations/Functions.swift
@@ -0,0 +1,8 @@
+//
+//  Functions.swift
+//  MyLocations
+//
+//  Created by Luis Faz on 2/11/24.
+//
+
+import Foundation
---
+++ MyLocations/HudView.swift
@@ -0,0 +1,8 @@
+//
+//  HudView.swift
+//  MyLocations
+//
+//  Created by Luis Faz on 2/11/24.
+//
+
+import Foundation
