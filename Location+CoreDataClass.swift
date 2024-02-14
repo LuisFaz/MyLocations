5dde4aa6295b7bcbfdadb1c1a3a7d0454b8cae79
LuisFaz <alejandro.faz@hotmail.com>
2024-02-13 19:34:26 -0500

    Added Database

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Luis Faz on 2/13/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
