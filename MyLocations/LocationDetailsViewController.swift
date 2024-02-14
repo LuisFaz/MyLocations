7d90551ff70a7685c2e3a1bf516631719612f62d
LuisFaz <alejandro.faz@hotmail.com>
2024-02-14 17:49:05 -0500

    Edit Locations

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -31,11 +31,30 @@ class LocationDetailsViewController: UITableViewController {
   var categoryName = "No Category"
   var managedObjectContext: NSManagedObjectContext!
   var date = Date()
+  var descriptionText = ""
+
+  var locationToEdit: Location? {
+    didSet {
+      if let location = locationToEdit {
+        descriptionText = location.locationDescription
+        categoryName = location.category
+        date = location.date
+        coordinate = CLLocationCoordinate2DMake(
+          location.latitude,
+          location.longitude)
+        placemark = location.placemark
+      }
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    descriptionTextView.text = ""
+    if let location = locationToEdit {
+      title = "Edit Location"
+    }
+
+    descriptionTextView.text = descriptionText
     categoryLabel.text = categoryName
 
     latitudeLabel.text = String(
@@ -73,9 +92,16 @@ class LocationDetailsViewController: UITableViewController {
     guard let mainView = navigationController?.parent?.view
       else { return }
     let hudView = HudView.hud(inView: mainView, animated: true)
-    hudView.text = "Tagged"
 
-    let location = Location(context: managedObjectContext)
+    let location: Location
+    if let temp = locationToEdit {
+      hudView.text = "Updated"
+      location = temp
+    } else {
+      hudView.text = "Tagged"
+      location = Location(context: managedObjectContext)
+    }
+
     location.locationDescription = descriptionTextView.text
     location.category = categoryName
     location.latitude = coordinate.latitude
